d5119301228fcd8f1f7422568dc66f6d1a9d8b27
Langning Chen <chenlangning2009@outlook.com>
2024-09-15 19:21:11 +0800

    uploadImage Temp

--- src/i18n.ts
+++ src/i18n.ts
@@ -125,10 +125,16 @@ const resources = {
       "changeInfo": "Change information",
       "studentUpdateSuccess": "Update student successfully",
       "logoutSuccess": "Logout successfully",
+      "uploadImage": "Upload image",
+      "dragToUpload": "Drag file here to upload",
+      "releaseToUpload": "Release to upload",
     },
   },
   zh: {
     translation: {
+      "dragToUpload": "拖动文件到此处上传",
+      "releaseToUpload": "释放以上传",
+      "uploadImage": "上传图片",
       "logoutSuccess": "登出成功",
       "404": "404 未找到",
       "404Description": "您要查找的页面未找到。",
